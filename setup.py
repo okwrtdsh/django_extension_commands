97c768893de222b6a44b662e45addf08f564ff63
YoKW <okawara.tadashi.0621@gmail.com>
2015-12-16 05:03:52 +0900

    setup.pyの修正

--- setup.py
+++ setup.py
@@ -21,13 +21,6 @@ def fullsplit(path, result=None):
         return result
     return fullsplit(head, [tail] + result)
 
-# Tell distutils to put the data_files in platform-specific installation
-# locations. See here for an explanation:
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
-
-
 # Compile the list of packages available, because distutils doesn't have
 # an easy way to do this.
 packages, package_data = [], {}
